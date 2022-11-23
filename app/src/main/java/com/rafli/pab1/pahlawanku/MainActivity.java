68c00cd947495cf6aff9121566cd7aa743c7d322
rafliansyahh <rafli.ansyahh@mhs.mdp.ac.id>
2022-11-23 16:41:47 +0700

    Finishhh Project

--- app/src/main/java/com/rafli/pab1/pahlawanku/MainActivity.java
+++ app/src/main/java/com/rafli/pab1/pahlawanku/MainActivity.java
@@ -1,11 +1,14 @@
 package com.rafli.pab1.pahlawanku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -22,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         rvPahlawan.setHasFixedSize(true);
 
         data.addAll(DataPahlawan.ambilDataPahlawan());
-//        tampilDataCard();
-        tampilDataGrid();
+        tampilDataCard();
+
     }
 
     private void tampilDataCard(){
@@ -36,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
         rvPahlawan.setLayoutManager(new GridLayoutManager(this,2));
         AdapterGrid AG = new AdapterGrid(data, MainActivity.this);
         rvPahlawan.setAdapter(AG);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tampilan,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_grid:
+                tampilDataGrid();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
